c26319b55cfebdf83791a3963d50556eb7393cf0
Frostflowerss <130375717+Frostflowerss@users.noreply.github.com>
2026-03-05 01:56:47 +0700

    Update profile.ts

--- src/resources/profile.ts
+++ src/resources/profile.ts
@@ -1,5 +1,5 @@
 export const contact = {
-  phone: "(+84) 888284998",
+  phone: "(+84)888284998",
   email: "pnt.architect.work@gmail.com",
   addressVI: "S.106 Vinhomes Smart City, Tây Mỗ, Nam Từ Liêm, Hà Nội",
   addressEN: "S.106 Vinhomes Smart City, Tay Mo, South Tu Liem, Ha Noi",
