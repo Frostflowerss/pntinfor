248d59c40478129d14f4026f0245890b41a0a3ba
Frostflowerss <130375717+Frostflowerss@users.noreply.github.com>
2026-06-18 12:43:29 +0700

    Delete src directory

--- src/resources/profile.ts
+++
@@ -1,7 +0,0 @@
-export const contact = {
-  phone: "0888284998",
-  email: "pnt.architect.work@gmail.com",
-  addressVI: "Vinhomes Smart City, Tây Mỗ, Nam Từ Liêm, Hà Nội",
-  addressEN: "Vinhomes Smart City, Tay Mo, South Tu Liem, Ha Noi",
-  cvPdfPath: "/files/PNT_CV_Architect_2025_VN-EN-2.pdf",
-};
