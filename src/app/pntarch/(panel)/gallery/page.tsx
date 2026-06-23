c4073d3d57a2200a1a0f628e913b9a5dd3635da2
Frostflowerss <130375717+Frostflowerss@users.noreply.github.com>
2026-06-24 00:34:28 +0700

    Delete src directory

--- src/app/pntarch/(panel)/gallery/page.tsx
+++
@@ -1,9 +0,0 @@
-import { adminGetGallery } from "@/lib/admin-data";
-import { GalleryManager } from "@/components/admin/GalleryManager";
-
-export const dynamic = "force-dynamic";
-
-export default async function GalleryAdmin() {
-  const images = await adminGetGallery();
-  return <GalleryManager images={images} />;
-}
