2133b32700c3e54af13cb20e94e176ecdbcca846
Frostflowerss <130375717+Frostflowerss@users.noreply.github.com>
2026-06-23 23:56:24 +0700

    Delete src directory

--- src/app/pntarch/(panel)/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import { adminGetProfile } from "@/lib/admin-data";
-import { ProfileForm } from "@/components/admin/ProfileForm";
-
-export const dynamic = "force-dynamic";
-
-export default async function ProfileAdmin() {
-  const profile = await adminGetProfile();
-  return <ProfileForm profile={profile} />;
-}
